1e376101ab0d81263f042cd9c68c92583e026e2b
Kari Huerta <kari.mckay@gaustincc.edu>
2023-12-31 17:50:01 -0600

    button distinction

--- js/aboutme.js
+++ js/aboutme.js
@@ -1,7 +1,6 @@
 
 $(document).ready(function() {
 	
-	$('div.details > *').hide();
 	$('input.aboutButton').click(function() {
 		var buttonId = $(this)[0].id;
 		hideShowDetails(buttonId);	
@@ -9,8 +8,15 @@ $(document).ready(function() {
 	
 });
 
+function defaultSettings()
+{
+	$('div.details > *').hide();
+}
+
 function hideShowDetails(elementID)
 {
 	$('div.details > *').hide();
+	$('div.buttons input[type="button"]').css({ "background-color":"#06194C" });
+	$('input#'+elementID).css({ "background-color":"#0C339B" });
 	$('div.details [id*="'+elementID+'"]').show();
 }
